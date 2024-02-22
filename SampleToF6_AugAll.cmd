89f90bdae31f228f89cbd07b4f1d5ccb4f40e711
elieppr <eliana.raizman@hotmail.com>
2024-02-21 20:57:10 -0800

    added sample to F6 aug all

---
+++ SampleToF6_AugAll.cmd
@@ -0,0 +1,7 @@
+python .\train_SampleToF6_AugAll.py --totalNumRuns 20 --augmentation none --sample 
+python .\train_SampleToF6_AugAll.py --totalNumRuns 20 --augmentation jitter --sample 
+python .\train_SampleToF6_AugAll.py --totalNumRuns 20 --augmentation blur_clahe --sample 
+python .\train_SampleToF6_AugAll.py --totalNumRuns 20 --augmentation clahe --sample 
+python .\train_SampleToF6_AugAll.py --totalNumRuns 20 --augmentation blur --sample 
+python .\train_SampleToF6_AugAll.py --totalNumRuns 20 --augmentation rotate --sample 
+python .\train_SampleToF6_AugAll.py --totalNumRuns 20 --augmentation brightness --sample 
